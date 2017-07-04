b1468f35ffa01d695b7a3a636697e29bcdba5066
Anthony Frugier <fru.anthony@gmail.com>
2017-07-04 11:28:47 +0200

    Tp Java 01

---
+++ Bonjour.java
@@ -0,0 +1,8 @@
+/* Cette class affiche 
+un texte prédefinie */
+public class Bonjour {
+    public static void main(String args[]) {
+        // affiche Bonjour tout le monde
+        System.out.println("Bonjour tout le monde");
+    }
+}
